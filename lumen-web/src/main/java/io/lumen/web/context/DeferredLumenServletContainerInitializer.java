2046fd72faccad4f071a64e8b326f430176171ad
David Stefanovic <david.stefanovic0711@gmail.com>
2026-06-08 15:12:04 +0200

    fix: added DeferredLumenInitializer so that WsFilter runs before LumenSecurityFilter for authentication

---
+++ lumen-web/src/main/java/io/lumen/web/context/DeferredLumenServletContainerInitializer.java
@@ -0,0 +1,20 @@
+package io.lumen.web.context;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+
+import java.util.Set;
+
+public class DeferredLumenServletContainerInitializer implements ServletContainerInitializer {
+
+    private final AnnotationWebApplicationContext lumenContext;
+
+    public DeferredLumenServletContainerInitializer(AnnotationWebApplicationContext lumenContext) {
+        this.lumenContext = lumenContext;
+    }
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) {
+        lumenContext.onDeferredStartup();
+    }
+}
